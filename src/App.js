5895383a6ed2ec4d66a786f112fb44d385c11320
Michael Salvati <print@3daddict.com>
2019-01-18 14:34:26 -0800

    created a basic example of get and storing data

--- src/App.js
+++ src/App.js
@@ -1,80 +1,53 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import './App.css';
 
-function BasicExample() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
+const products = [
+    {
+        name: 'iPad',
+        price: 200
+    },
+    {
+        name: 'iPhone',
+        price: 650
+    }
 
-        <hr />
+];
 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         products: ''
+      }
+    }
+    
+
+    componentWillMount() {
+        const products = JSON.parse(localStorage.getItem('products'));
+        this.setState({
+            products: products
+        })
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+        {
+            this.state.products.map((product) => {
+                return (
+                    <div key={product.name}>
+                        <span>{product.name}</span> | <span>{product.price}</span>
+                    </div>
+                );
+            })
+        }
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
-
-export default BasicExample;
+export default App;
