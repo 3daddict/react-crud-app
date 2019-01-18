8d6e91d3f2107e52ad2281884d863459432853dd
Michael Salvati <print@3daddict.com>
2019-01-18 14:51:36 -0800

    delete product function created with refactored code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ProductItem from "./ProductItem";
 
 const products = [
-    {
-        name: 'iPad',
-        price: 200
-    },
-    {
-        name: 'iPhone',
-        price: 650
-    }
-
+  {
+    name: "iPad",
+    price: 200
+  },
+  {
+    name: "iPhone",
+    price: 650
+  }
 ];
 
-localStorage.setItem('products', JSON.stringify(products));
+localStorage.setItem("products", JSON.stringify(products));
 
 class App extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         products: ''
-      }
-    }
-    
+  constructor(props) {
+    super(props);
 
-    componentWillMount() {
-        const products = JSON.parse(localStorage.getItem('products'));
-        this.setState({
-            products: products
-        })
-    }
+    this.state = {
+      products: JSON.parse(localStorage.getItem("products"))
+    };
+    this.onDelete = this.onDelete.bind(this);
+  }
+
+  componentWillMount() {
+    const products = this.getProducts();
+
+    this.setState({
+      products: products
+    });
+  }
+
+  getProducts() {
+    return this.state.products;
+  }
+
+  onDelete(name) {
+    const products = this.getProducts();
+    const filteredProducts = products.filter(product => {
+        return product.name !== name;
+    })
+    
+    this.setState({
+        products: filteredProducts
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Products Manager</h1>
-        {
-            this.state.products.map((product) => {
-                return (
-                    <div key={product.name}>
-                        <span>{product.name}</span> | <span>{product.price}</span>
-                    </div>
-                );
-            })
-        }
+        {this.state.products.map(product => {
+          return (
+            <ProductItem
+              key={product.name}
+              {...product}
+              onDelete={this.onDelete}
+            />
+          );
+        })}
       </div>
     );
   }
---
+++ src/ProductItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class ProductItem extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+      this.onDelete = this.onDelete.bind(this);
+    }
+
+    onDelete() {
+        const { onDelete, name } = this.props;
+
+        onDelete(name);
+    }
+    
+  render() {
+    const { name, price } = this.props;
+
+    return (
+      <div>
+        <span>{name}</span> | <span>{price}</span>
+        <button onClick={this.onDelete}>Delete</button>
+      </div>
+    );
+  }
+}
+
+export default ProductItem;
