53537fec31c31d5dd560054e7ab0e1394ddefbdb
Michael Salvati <print@3daddict.com>
2019-01-18 13:25:41 -0800

    example dropping component into another

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MyComponent from './MyComponent';
 import './App.css';
 
 class App extends Component {
@@ -25,6 +26,7 @@ class App extends Component {
       <div className="App">
         <h1>{this.state.title}</h1>
         <div onClick={this.onClick}>Click Here</div>
+        <MyComponent />
       </div>
     );
   }
---
+++ src/MyComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class MyComponent extends Component {
+  render() {
+
+    return (
+      <div className="component">
+        <h1>This is a Child component</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
